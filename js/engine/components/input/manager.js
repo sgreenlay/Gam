945dc4d8fe9f6fbab2e393970a7a5796bd5699ed
Scott Greenlay <sgreenlay@gmail.com>
2014-03-08 08:23:46 -0800

    implemented touch input

--- js/engine/components/input/manager.js
+++ js/engine/components/input/manager.js
@@ -8,11 +8,28 @@ namespace('sg.gam.components.engine.input.manager', (function() {
 
 	var manager = function(canvas) {
 		this.canvas = canvas;
-		
-		this.mouse = new sg.gam.components.engine.input.mouse(canvas);
+
+		this.mouse = new sg.gam.components.engine.input.mouse(this, canvas);
 		this.keyboard = new sg.gam.components.engine.input.keyboard(canvas);
 		this.gamepad = new sg.gam.components.engine.input.gamepad(canvas);
-		this.touch = new sg.gam.components.engine.input.touch(canvas);
+		this.touch = new sg.gam.components.engine.input.touch(this, canvas);
+	};
+
+	manager.prototype.map_input_point = function(point) {
+		var self = this;
+
+		var viewport_width = self.canvas.clientWidth;
+		var viewport_height = self.canvas.clientHeight;
+
+		var relative_point = {
+			x : point.x / viewport_width,
+			y : point.y / viewport_height
+		};
+
+		return {
+			x : relative_point.x * self.canvas.width,
+			y : relative_point.y * self.canvas.height
+		};
 	};
 
 	manager.prototype.capture_input = function capture_input() {
@@ -20,33 +37,33 @@ namespace('sg.gam.components.engine.input.manager', (function() {
 		this.keyboard.capture_input();
 		this.gamepad.capture_input();
 		this.touch.capture_input();
-		
+
 		this.canvas.onselectstart = this.onselectstart();
 	};
-	
+
 	manager.prototype.release_input = function release_input() {
 		this.canvas.onselectstart = null;
-		
+
 		this.mouse.release_input();
 		this.keyboard.release_input();
 		this.gamepad.release_input();
 		this.touch.release_input();
 	}
-	
+
 	manager.prototype.reset = function reset() {
 		this.mouse.reset();
 		this.keyboard.reset();
 		this.gamepad.reset();
 		this.touch.reset();
 	};
-	
+
 	manager.prototype.update = function update() {
 		this.gamepad.update();
 	};
-	
+
 	manager.prototype.peek = function peek_next_event() {
 		var evt = null;
-		
+
 		if (evt == null) {
 			evt = this.mouse.peek();
 		}
@@ -59,13 +76,13 @@ namespace('sg.gam.components.engine.input.manager', (function() {
 		if (evt == null) {
 			evt = this.touch.peek();
 		}
-		
+
 		return evt;
 	};
-	
+
 	manager.prototype.next = function next_event() {
 		var evt = null;
-		
+
 		if (evt == null) {
 			evt = this.mouse.next();
 		}
@@ -78,10 +95,10 @@ namespace('sg.gam.components.engine.input.manager', (function() {
 		if (evt == null) {
 			evt = this.touch.next();
 		}
-		
+
 		return evt;
 	};
-	
+
 	manager.prototype.onselectstart = function onselectstart() {
 		var onselectstarthandler = function() {
 			return false;
--- js/engine/components/input/touch.js
+++ js/engine/components/input/touch.js
@@ -1,26 +1,58 @@
 
 namespace('sg.gam.components.engine.input.touch', (function() {
 
-	var touch = function(canvas) {
+	var touch = function(manager, canvas) {
+		this.manager = manager;
 		this.canvas = canvas;
 		this.event_queue = new Array();
 	};
 
 	touch.prototype.capture_input = function capture_input() {
-		this.ontouchstart = this.touchstart();
-		this.ontouchend = this.touchend();
-		this.ontouchcancel = this.touchcancel();
-		this.ontouchleave = this.touchleave();
-		this.ontouchmove = this.touchmove();
-		
-		this.canvas.addEventListener("touchstart", this.ontouchstart, false);
-		this.canvas.addEventListener("touchend", this.ontouchend, false);
-		this.canvas.addEventListener("touchcancel", this.ontouchcancel, false);
-		this.canvas.addEventListener("touchleave", this.ontouchleave, false);
-		this.canvas.addEventListener("touchmove", this.ontouchmove, false);
+		if (window.PointerEvent) {
+			this.onpointerenter = this.pointerenter();
+			this.onpointerleave = this.pointerleave();
+			this.onpointermove = this.pointermove();
+			this.onpointerdown = this.pointerdown();
+			this.onpointerup = this.pointerup();
+
+			this.canvas.addEventListener("pointerenter", this.onpointerenter, false);
+			this.canvas.addEventListener("pointerleave", this.onpointerleave, false);
+			this.canvas.addEventListener("pointermove", this.onpointermove, false);
+			this.canvas.addEventListener("pointerdown", this.onpointerdown, false);
+			this.canvas.addEventListener("pointerup", this.onpointerup, false);
+		}
+		else {
+			this.ontouchstart = this.touchstart();
+			this.ontouchend = this.touchend();
+			this.ontouchmove = this.touchmove();
+
+			this.canvas.addEventListener("touchstart", this.ontouchstart, false);
+			this.canvas.addEventListener("touchend", this.ontouchend, false);
+			this.canvas.addEventListener("touchmove", this.ontouchmove, false);
+		}
 	};
-	
+
 	touch.prototype.release_input = function release_input() {
+		if (this.onpointerenter) {
+			this.canvas.removeEventListener("pointerenter", this.onpointerenter, false);
+			this.onpointerenter = null;
+		}
+		if (this.onpointerleave) {
+			this.canvas.removeEventListener("pointerleave", this.onpointerleave, false);
+			this.onpointerleave = null;
+		}
+		if (this.onpointermove) {
+			this.canvas.removeEventListener("pointermove", this.onpointermove, false);
+			this.onpointermove = null;
+		}
+		if (this.onpointerdown) {
+			this.canvas.removeEventListener("pointerdown", this.onpointerdown, false);
+			this.onpointerdown = null;
+		}
+		if (this.onpointerup) {
+			this.canvas.removeEventListener("pointerup", this.onpointerup, false);
+			this.onpointerup = null;
+		}
 		if (this.ontouchstart) {
 			this.canvas.removeEventListener("touchstart", this.ontouchstart, false);
 			this.ontouchstart = null;
@@ -29,39 +61,31 @@ namespace('sg.gam.components.engine.input.touch', (function() {
 			this.canvas.removeEventListener("touchend", this.ontouchend, false);
 			this.ontouchsend = null;
 		}
-		if (this.ontouchcancel) {
-			this.canvas.removeEventListener("touchcancel", this.ontouchcancel, false);
-			this.ontouchcancel = null;
-		}
-		if (this.ontouchleave) {
-			this.canvas.removeEventListener("touchleave", this.ontouchleave, false);
-			this.ontouchleave = null;
-		}
 		if (this.ontouchmove) {
 			this.canvas.removeEventListener("touchmove", this.ontouchmove, false);
 			this.ontouchmove = null;
 		}
 	}
-	
+
 	touch.prototype.reset = function reset() {
 		while (this.next()) {
 			// do nothing
 		}
-		
+
 		// TODO
 	};
-	
+
 	touch.prototype.poll = function poll() {
-		
+
 		// TODO
-		
+
 		return null;
 	};
-	
+
 	touch.prototype.is_valid_event = function is_valid_event(event) {
 		return true;
 	};
-	
+
 	touch.prototype.peek = function peek_next_event() {
 		while (this.event_queue && this.event_queue.length > 0) {
 			if (!this.is_valid_event(this.event_queue[0])) {
@@ -73,7 +97,7 @@ namespace('sg.gam.components.engine.input.touch', (function() {
 		}
 		return null;
 	};
-	
+
 	touch.prototype.next = function next_event() {
 		while (this.event_queue && this.event_queue.length > 0) {
 			if (!this.is_valid_event(this.event_queue[0])) {
@@ -81,63 +105,236 @@ namespace('sg.gam.components.engine.input.touch', (function() {
 			}
 			else {
 				var event = this.event_queue.shift();
-				
+
 				// TODO
-				
+
 				return event;
 			}
 		}
 		return null;
 	};
-	
+
 	touch.prototype.touchstart = function touchstart() {
 		var self = this;
 		var ontouchstart = function(evt) {
 			evt.preventDefault();
-			
-			// TODO
+
+			for (var i = 0; i < evt.changedTouches.length; i++) {
+				var touch = evt.changedTouches[i];
+
+				var event = {
+					type : 'touch',
+					state : 'enter',
+					time : new Date(),
+					id : touch.identifier,
+					position : self.manager.map_input_point({
+						x : touch.pageX - evt.target.offsetLeft,
+						y : touch.pageY - evt.target.offsetTop
+					})
+				};
+				self.event_queue.push(event);
+
+				event = {
+					type : 'touch',
+					state : 'down',
+					time : new Date(),
+					id : touch.identifier,
+					position : self.manager.map_input_point({
+						x : touch.pageX - evt.target.offsetLeft,
+						y : touch.pageY - evt.target.offsetTop
+					})
+				};
+
+				self.event_queue.push(event);
+			}
+
+			evt.cancelBubble = true;
 		};
 		return ontouchstart;
 	};
-	
+
 	touch.prototype.touchend = function ontouchend(evt) {
 		var self = this;
 		var ontouchend = function(evt) {
 			evt.preventDefault();
-			
-			// TODO
+
+			for (var i = 0; i < evt.changedTouches.length; i++) {
+				var touch = evt.changedTouches[i];
+
+				var event = {
+					type : 'touch',
+					state : 'up',
+					time : new Date(),
+					id : touch.identifier,
+					position : self.manager.map_input_point({
+						x : touch.pageX - evt.target.offsetLeft,
+						y : touch.pageY - evt.target.offsetTop
+					})
+				};
+				self.event_queue.push(event);
+
+				event = {
+					type : 'touch',
+					state : 'exit',
+					time : new Date(),
+					id : touch.identifier,
+					position : self.manager.map_input_point({
+						x : touch.pageX - evt.target.offsetLeft,
+						y : touch.pageY - evt.target.offsetTop
+					})
+				};
+				self.event_queue.push(event);
+			}
+
+			evt.cancelBubble = true;
 		};
 		return ontouchend;
 	};
-	
-	touch.prototype.touchcancel = function ontouchcancel(evt) {
-		var self = this;
-		var ontouchcancel = function(evt) {
-			evt.preventDefault();
-			
-			// TODO
-		};
-		return ontouchcancel;
-	};
-	
-	touch.prototype.touchleave = function ontouchleave(evt) {
-		var self = this;
-		var ontouchleave = function(evt) {
-			evt.preventDefault();
-			
-			// TODO
-		};
-		return ontouchleave;
-	};
-	
+
 	touch.prototype.touchmove = function ontouchmove(evt) {
 		var self = this;
 		var ontouchmove = function(evt) {
 			evt.preventDefault();
-			
-			// TODO
+
+			for (var i = 0; i < evt.changedTouches.length; i++) {
+				var touch = evt.changedTouches[i];
+
+				var event = {
+					type : 'touch',
+					state : 'move',
+					time : new Date(),
+					id : touch.identifier,
+					position : self.manager.map_input_point({
+						x : touch.pageX - evt.target.offsetLeft,
+						y : touch.pageY - evt.target.offsetTop
+					})
+				};
+				self.event_queue.push(event);
+			}
+
+			evt.cancelBubble = true;
 		};
 		return ontouchmove;
+	};
+
+	touch.prototype.pointerenter = function pointerenter(evt) {
+		var self = this;
+		var onpointerenter = function(evt) {
+			if (evt.pointerType == "touch") {
+				evt.preventDefault();
+
+				var event = {
+					type : 'touch',
+					state : 'enter',
+					time : new Date(),
+					id : evt.pointerId,
+					position : self.manager.map_input_point({
+						x : evt.pageX - evt.target.offsetLeft,
+						y : evt.pageY - evt.target.offsetTop
+					})
+				};
+				self.event_queue.push(event);
+
+				evt.cancelBubble = true;
+			}
+		};
+		return onpointerenter;
+	};
+
+	touch.prototype.pointerleave = function pointerleave(evt) {
+		var self = this;
+		var onpointerleave = function(evt) {
+			if (evt.pointerType == "touch") {
+				evt.preventDefault();
+
+				var event = {
+					type : 'touch',
+					state : 'exit',
+					time : new Date(),
+					id : evt.pointerId,
+					position : self.manager.map_input_point({
+						x : evt.pageX - evt.target.offsetLeft,
+						y : evt.pageY - evt.target.offsetTop
+					})
+				};
+				self.event_queue.push(event);
+
+				evt.cancelBubble = true;
+			}
+		};
+		return onpointerleave;
+	};
+
+	touch.prototype.pointermove = function pointermove(evt) {
+		var self = this;
+		var onpointermove = function(evt) {
+			if (evt.pointerType == "touch") {
+				evt.preventDefault();
+
+				var event = {
+					type : 'touch',
+					state : 'move',
+					time : new Date(),
+					id : evt.pointerId,
+					position : self.manager.map_input_point({
+						x : evt.pageX - evt.target.offsetLeft,
+						y : evt.pageY - evt.target.offsetTop
+					})
+				};
+				self.event_queue.push(event);
+
+				evt.cancelBubble = true;
+			}
+		};
+		return onpointermove;
+	};
+
+	touch.prototype.pointerdown = function pointerdown(evt) {
+		var self = this;
+		var onpointerdown = function(evt) {
+			if (evt.pointerType == "touch") {
+				evt.preventDefault();
+
+				var event = {
+					type : 'touch',
+					state : 'down',
+					time : new Date(),
+					id : evt.pointerId,
+					position : self.manager.map_input_point({
+						x : evt.pageX - evt.target.offsetLeft,
+						y : evt.pageY - evt.target.offsetTop
+					})
+				};
+				self.event_queue.push(event);
+
+				evt.cancelBubble = true;
+			}
+		};
+		return onpointerdown;
+	};
+
+	touch.prototype.pointerup = function pointerup(evt) {
+		var self = this;
+		var onpointerup = function(evt) {
+			if (evt.pointerType == "touch") {
+				evt.preventDefault();
+
+				var event = {
+					type : 'touch',
+					state : 'up',
+					time : new Date(),
+					id : evt.pointerId,
+					position : self.manager.map_input_point({
+						x : evt.pageX - evt.target.offsetLeft,
+						y : evt.pageY - evt.target.offsetTop
+					})
+				};
+				self.event_queue.push(event);
+
+				evt.cancelBubble = true;
+			}
+		};
+		return onpointerup;
 	};
 
 	return touch;
