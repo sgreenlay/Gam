2f90725374d4e2a501e09cd9edf62c0b1294f852
Scott Greenlay <sgreenlay@gmail.com>
2019-10-04 16:39:56 -0700

    small updates

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ let size = { width: canvas.width, height: canvas.height };
 class Engine {
     constructor() {
         this.systems = new Array();
-        this.objects = new Array();
+        this.entities = new Array();
     }
 
     update(dt, time) {
-        this.systems.forEach(system => system.update(dt, time, this.objects));
+        this.systems.forEach(system => system.update(dt, time, this.entities));
     }
 
     run() {
@@ -32,12 +32,12 @@ class Engine {
         this.systems.push(system);
     }
 
-    addObject(object) {
-        this.objects.push(object);
+    addEntity(entity) {
+        this.entities.push(entity);
     }
 }
 
-class GameObject {
+class Entity {
     constructor() {
         this.components = new Array();
     }
@@ -58,21 +58,17 @@ class GameObject {
     }
 }
 
-class RenderComponent {
+class Bounds {
     constructor(x, y, w, h) {
-        this.bounds = { 
-            x : x,
-            y : y,
-            w : w,
-            h : h
-        };
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
     }
+}
 
-    render(ctx) {
-        // Draw a rectangle
-        ctx.fillStyle = 'rgba(255, 0, 0, 1.0)';
-        ctx.fillRect(this.bounds.x, this.bounds.y, this.bounds.w, this.bounds.h);
-    }
+class Renderable {
+    constructor() {}
 }
 
 class RenderSystem {
@@ -85,18 +81,27 @@ class RenderSystem {
         this.ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
     }
 
-    update(dt, time, objects) {
+    update(dt, time, entities) {
         // Fill background
         this.ctx.globalAlpha = 1;
         this.ctx.fillStyle = "#ffffff";
         this.ctx.fillRect(0, 0, size.width, size.height);
 
-        // Iterate through all the renderable objects and render them
-        for (var i = 0; i < objects.length; ++i) {
-            let object = objects[i];
-            let renderer = object.getComponent(RenderComponent);
-            if (renderer) {
-                renderer.render(this.ctx);
+        // Iterate through all the renderable entities and render them
+        for (var i = 0; i < entities.length; ++i) {
+            let entity = entities[i];
+
+            let renderable = entity.getComponent(Renderable);
+            let bounds = entity.getComponent(Bounds);
+
+            if (renderable && bounds) {
+                // Draw a rectangle
+                this.ctx.fillStyle = 'rgba(255, 0, 0, 1.0)';
+                this.ctx.fillRect(
+                    bounds.x,
+                    bounds.y,
+                    bounds.w,
+                    bounds.h);
             }
         }
     }
@@ -107,8 +112,9 @@ const engine = new Engine();
 const renderer = new RenderSystem(canvas);
 engine.addSystem(renderer);
 
-const box = new GameObject();
-box.addComponent(new RenderComponent(size.width / 2 - 20, size.height / 2 - 20, 40, 40));
-engine.addObject(box);
+const box = new Entity();
+box.addComponent(new Bounds(size.width / 2 - 20, size.height / 2 - 20, 40, 40));
+box.addComponent(new Renderable());
+engine.addEntity(box);
 
 engine.run();
